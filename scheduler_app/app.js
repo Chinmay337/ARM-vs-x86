0e06be24addb28431fd9d08d3949ba098b3f4443
Chinmay337 <65412787+Chinmay337@users.noreply.github.com>
2022-11-07 22:14:20 -0500

    logic update

--- scheduler_app/app.js
+++ scheduler_app/app.js
@@ -8,15 +8,17 @@ const INTEL_FLOAT_JS = process.env.INTEL_FLOAT_JS;
 const INTEL_INT_JS = process.env.INTEL_INT_JS;
 
 const endpoints = [
-  ARM_FLOAT_GO,
-  ARM_INT_GO,
-  INTEL_FLOAT_GO,
-  INTEL_INT_GO,
-  ARM_FLOAT_JS,
-  ARM_INT_JS,
-  INTEL_FLOAT_JS,
-  INTEL_INT_JS,
+  process.env.ARM_FLOAT_GO,
+  process.env.ARM_INT_GO,
+  process.env.INTEL_FLOAT_GO,
+  process.env.INTEL_INT_GO,
+  process.env.ARM_FLOAT_JS,
+  process.env.ARM_INT_JS,
+  process.env.INTEL_FLOAT_JS,
+  process.env.INTEL_INT_JS,
 ];
+
+const burstDuration = 15 * 60 * 1000; // 15 minutes in Milliseconds = 15m * 60s * 1000ms
 
 const makeRequest = async (endpoint) => {
   // await fetch endpoint
@@ -25,7 +27,7 @@ const makeRequest = async (endpoint) => {
 const currentTime = new Date();
 
 const burstEndpoints = async (startTime) => {
-  while (new Date() - startTime < 15 * 60) {
+  while (new Date() - startTime < burstDuration) {
     const res = await Promise.all(endpoints.map(makeRequest));
     const data = res.map((res) => res.data);
   }
